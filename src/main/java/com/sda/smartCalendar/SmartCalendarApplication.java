af7b24ec4a805795aa1603db262b00eeeb485359
gwitczak <grzegorz.witczak@protonmail.com>
2018-10-03 21:05:25 +0200

    Initial commit - Spring initializr

---
+++ src/main/java/com/sda/smartCalendar/SmartCalendarApplication.java
@@ -0,0 +1,12 @@
+package com.sda.smartCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCalendarApplication.class, args);
+	}
+}
