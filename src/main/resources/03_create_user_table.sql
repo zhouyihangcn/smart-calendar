24f35f05de324155c4115cd344dd510ba06bd981
GosiaBakM <machmalgorzata@gmail.com>
2018-10-12 08:55:00 +0200

    Add feature-google-release-scaffolding

--- src/main/resources/03_create_user_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS user (
-  id          VARCHAR(36) primary key,
-  nick        VARCHAR(20),
-  password   VARCHAR(64)
-);
--- src/main/resources/05_users_roles.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users_roles (
-  user_id   VARCHAR(36) not null,
-  role_id   VARCHAR(36) not null,
-  FOREIGN KEY (user_id) REFERENCES user(id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  UNIQUE (user_id, role_id)
-);
